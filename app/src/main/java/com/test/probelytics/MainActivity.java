911b0b8ee6e641f7089a0fddd3e70d85b182009a
dimasa07 <dimasagung35bdg76@gmail.com>
2021-07-13 00:44:43 +0700

    Fixed

--- app/src/main/java/com/test/probelytics/MainActivity.java
+++ app/src/main/java/com/test/probelytics/MainActivity.java
@@ -1,15 +1,12 @@
 package com.test.probelytics;
 
-import android.app.*;
 import android.os.*;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity 
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-				
-    }
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
